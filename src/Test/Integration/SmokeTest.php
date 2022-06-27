44889daba65bcbcc350c86a30a670f8085dbce48
Damien Retzinger <damienwebdev@gmail.com>
2022-06-27 10:07:14 -0400

    ci: add unit, install, integration test

---
+++ src/Test/Integration/SmokeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MagePsycho\StorePricing\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class SmokeTest extends TestCase
+{
+    public function testItworks()
+    {
+        $this->assertEquals(true, true);
+    }
+}
